77def6e1b1ded46a7975f24aec47a75f511f24c9
Carbon6090 <osotoff.stepan@yandex.ru>
2020-02-20 18:37:07 +0300

    add some functions

--- Polynom.hpp
+++ Polynom.hpp
@@ -7,11 +7,24 @@ class Polynom {
 	double *coef; // коэффициенты полинома
 	int n; // степень полинома
 public:
-	Polynom(int n);
+	Polynom(int n = 0);
+	Polynom(const Polynom& polynom);
+
+	Polynom& operator=(const Polynom& polynom);
+
 	void Read();
 
 	double Evaluate(double x) const;
 	Polynom Derivative() const;
+	int GetDegree() const;
+
+	Polynom operator*(const Polynom& polynom) const;
+	Polynom operator+(const Polynom& polynom) const;
+	Polynom operator-(const Polynom& polynom) const;
+	double operator[](int index) const;
+
+	void SetCoef(int index, double c);
+
 	~Polynom();
 
 	friend ostream& operator<<(ostream& os, const Polynom& polynom);
@@ -23,6 +36,25 @@ Polynom::Polynom(int n) {
 
 	for (int i = 0; i <= n; i++)
 		coef[i] = 0;
+}
+
+Polynom::Polynom(const Polynom& polynom) {
+	n = polynom.n;
+	coef = new double[polynom.n + 1];
+
+	for (int i = 0; i <= polynom.n; i++)
+		coef[i] = polynom.coef[i];
+}
+
+Polynom& Polynom::operator=(const Polynom& polynom) {
+	delete[] coef;
+	n = polynom.n;
+	coef = new double[polynom.n + 1];
+
+	for (int i = 0; i <= polynom.n; i++)
+		coef[i] = polynom.coef[i];
+
+	return *this;
 }
 
 void Polynom::Read() {
@@ -47,14 +79,105 @@ Polynom Polynom::Derivative() const {
 	return polynom;
 }
 
+int Polynom::GetDegree() const{
+	return n;
+}
+
+Polynom Polynom::operator*(const Polynom& polynom) const {
+	Polynom res(n + polynom.n);
+
+	for (int i = 0; i <= n; i++)
+		for (int j = 0; j <= polynom.n; j++)
+			res.coef[i + j] += coef[i] * polynom.coef[j];
+
+	return res;
+}
+
+Polynom Polynom::operator+(const Polynom& polynom) const {
+    Polynom res((n > polynom.n ? n : polynom.n) + 1);
+
+    for (int i = 0; i <= n; i++)
+    	res.coef[i] = coef[i];
+
+    for (int i = 0; i <= polynom.n; i++)
+    	res.coef[i] += polynom.coef[i];
+
+    return res;
+}
+
+Polynom Polynom::operator-(const Polynom& polynom) const {
+	Polynom res((n > polynom.n ? n : polynom.n) + 1);
+
+    for (int i = 0; i <= n; i++)
+    	res.coef[i] = coef[i];
+
+    for (int i = 0; i <= polynom.n; i++)
+    	res.coef[i] -= polynom.coef[i];
+
+    return res;
+}
+
+double Polynom::operator[](int index) const {
+	return coef[index];
+}
+
+void Polynom::SetCoef(int index, double c) {
+	coef[index] = c;
+}
 
 Polynom::~Polynom() {
 	delete[] coef;
 }
 
 ostream& operator<<(ostream& os, const Polynom& polynom) {
-	for (int i = polynom.n; i >= 0; i--)
-		os << polynom.coef[i] << " ";
+	bool empty;
+	for (int i = 0; i <= polynom.n; i++)
+		if (polynom.coef[i] != 0)
+			empty = false;
+
+	if (empty || polynom.n == 0)
+		cout << "0" << endl;
+
+	else {
+		for (int i = polynom.n; i > 0; i--) {
+
+			if (polynom.coef[i] == 0)
+				continue;
+
+			if (polynom.coef[i] > 0){
+				if (i == 1){
+					if (polynom.coef[i] == 1)
+						os << "x";
+					else
+						os << polynom.coef[i] << "x"; 
+				}
+				else {
+					if (polynom.coef[i] == 1)
+						os << "x^" << i;
+					else
+						os << polynom.coef[i] << "x^" << i; 
+				}
+
+			}
+			else{
+				if (i == 1){
+					if (polynom.coef[i] == -1)
+						os << "-x";
+					else
+						os << polynom.coef[i] << "x"; 
+				}
+				else {
+					if (polynom.coef[i] == -1)
+						os << "-x^" << i;
+					else
+						os << polynom.coef[i] << "x^" << i; 
+				}
+			}
+			if (polynom.coef[i - 1] > 0)
+				cout << "+";
+		}
+		os << polynom.coef[0];
+	}
 
 	return os << endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,22 +1,131 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include "Polynom.hpp"
 
 using namespace std;
 
-int main(){
-	int n; // степень полинома
-	cout << "Enter degree: ";
-	cin >> n;
-	Polynom polynom(n);
-	
-	cout << "Enter polynom f(x): ";
-	polynom.Read();
-	cout << polynom;
-	
-	double x;
-	cout << "Enter x: ";
-	cin >> x;
-	cout << "f(" << x << ") = " <<  polynom.Evaluate(x) << endl;
+struct Pol{
+	string name;
+	Polynom polynom;
+};
 
-	cout << polynom.Derivative() << endl;
+void Print(const vector<Pol> &pols){
+	if (pols.size() == 0)
+		cout << "There is no pols" << endl;
+	else
+		for (int i = 0; i < pols.size(); i++)
+			cout << pols[i].name << ": " << pols[i].polynom << endl;
+}
+
+Polynom Find(string name, const vector<Pol> &pols){
+	for (int i = 0; i < pols.size(); i++)
+		if (name == pols[i].name)
+			return pols[i].polynom;
+	
+	throw "There is no mathes";
+}
+
+void Menu() {
+	cout << "1. Add polynom" << endl;
+	cout << "2. Print polynom" << endl;
+	cout << "3. Find polynom by name" << endl;
+	cout << "4. Find the derivative" << endl;
+	cout << "5. To summarize 2 polynoms" << endl;
+	cout << "6. To substract 2 polynoms" << endl;
+	cout << "7. To multiply 2 polynoms" << endl;
+    cout << "8. Quit" << endl;
+}
+
+int main() {
+    int item;
+    vector<Pol> pols;
+    Polynom res, polynom;
+    int degree;
+    string name;
+    string s;
+    string s1;
+    do {
+        cout << "What do you want to do?" << endl;
+        Menu();
+        cout << ">";
+        cin >> item;
+
+        while(item < 1 || item > 7) {
+            cout << "Incorrect item. Try again: ";
+            cin >> item;
+        }
+
+        switch (item) {
+            case 1:
+                cout << "Enter polynom's name:";
+                cin >> name;          	
+            	
+                cout << "Enter degree: ";
+                cin >> degree;
+               	polynom = Polynom(degree);
+
+                cout << "Enter coeffs: ";
+                polynom.Read();
+                pols.push_back({name, polynom});
+                break;
+
+            case 2:
+                Print(pols);
+                break;
+            case 3:
+                cout << "Enter polynom's name: ";
+                cin >> name;
+                cout << Find(name, pols);
+                break;
+            case 4:
+                cout << "Enter the name of the polynom, which derivative you want to find: ";
+                cin >> name;
+                cout << Find(name, pols).Derivative();
+                break;
+            case 5:
+            	cout << "Enter names of polynoms: " << endl;
+            	cin >> s;
+            	cin >> s1;
+            	res = Find(s, pols) + Find(s1, pols);
+                cout << "Do you want to save this polynom? (yes/no)" << endl;
+                cin >> s; 
+                if (s == "yes"){
+	                string name;
+	                cout << "Enter polynom's name:";
+	                cin >> name;
+	                pols.push_back({name, res});
+                }
+                break;
+            case 6:
+           		cout << "Enter names of polynoms: " << endl;
+            	cin >> s;
+            	cin >> s1;
+            	res = Find(s, pols) + Find(s1, pols);
+                cout << "Do you want to save this polynom? (yes/no)" << endl;
+                cin >> s; 
+                if (s == "yes"){
+	                string name;
+	                cout << "Enter polynom's name:";
+	                cin >> name;
+	                pols.push_back({name, res});
+                }
+                break;  
+            case 7:
+            	cout << "Enter names of polynoms: " << endl;
+            	cin >> s;
+            	cin >> s1;
+            	res = Find(s, pols) + Find(s1, pols);
+                cout << "Do you want to save this polynom? (yes/no)" << endl;
+                cin >> s; 
+                if (s == "yes"){
+	                string name;
+	                cout << "Enter polynom's name:";
+	                cin >> name;
+	                pols.push_back({name, res});
+                }
+            	break;
+        }
+    } while (item != 8);
+
 }
